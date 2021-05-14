3e3acb390b781f07110087d4e979fc909ed168ec
Kate McColl <kate@katemccollart.com>
2021-05-14 13:04:26 +1000

    add: inital promt

--- employeeTracker.js
+++ employeeTracker.js
@@ -20,3 +20,19 @@ connection.connect((err) => {
     cmsSearch();
 });
 
+const cmsSearch = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'what would you like to do?',
+            choices: [
+                'Add departments, roles, employees',
+                'View departments, roles, employees',
+                'Update employee roles',
+                'Delete departments, roles, and employees',
+                'View the total utilized budget of a department',
+                'Exit'
+            ]
+        });
+};
